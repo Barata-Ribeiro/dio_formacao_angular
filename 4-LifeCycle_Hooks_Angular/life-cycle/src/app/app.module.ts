9e9a5827eda31adc0e9a9ea21b796747b915aece
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-17 07:27:40 -0300

    intro to component life cycle

--- 4-LifeCycle_Hooks_Angular/life-cycle/src/app/app.module.ts
+++ 4-LifeCycle_Hooks_Angular/life-cycle/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+// import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TitleComponent],
+  imports: [BrowserModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
