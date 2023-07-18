0117cd35a5f01daaba37a7d35f478cb5cb82e41b
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-18 06:31:40 -0300

    Structural Directives

--- 5-Directives/directives-project/src/app/card/card.component.ts
+++ 5-Directives/directives-project/src/app/card/card.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  products: string[] = [];
+  menuType: string = '';
 
+  constructor() {
+    this.products = ['mice', 'keyboard', 'laptop', 'mousepad'];
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+
+  addStuff() {
+    this.products.push('GPU');
+  }
+
+  remove(index: number) {
+    this.products.splice(index, 1);
+  }
 }
