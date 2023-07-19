b3c955eee68b8ad9fa9a20eafc145dd20ec16f0b
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-19 06:47:47 -0300

    Angular Modules

---
+++ 6-modules/proj-modules/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { NewListModule } from './new-list/new-list.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NewListModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
